041ab60925e5dc146015088b42d2cf4cec146100
marinakelana <eyin_77@yahoo.co.id>
2020-10-08 21:21:12 +0700

    Splash Screen, Nurullah Marina Kelana

--- app/src/main/java/com/example/srikandi/Dashboard.java
+++ app/src/main/java/com/example/srikandi/Dashboard.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
     }
 }
