2e299b61b73e07e8909cffb204b00bf963f5416c
marinakelana <eyin_77@yahoo.co.id>
2020-10-08 20:11:18 +0700

    Splash Screen, Nurullah Marina Kelana

---
+++ app/src/main/java/com/example/srikandi/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.example.srikandi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
